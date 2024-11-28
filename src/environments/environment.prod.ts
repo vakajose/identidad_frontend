140a126aba5dc7cf5b974f95fb7ff4ee78eb347b
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-11-27 20:40:03 -0400

    format

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: true,
-        contractAddress: '0xc77008489f97994f29fd86b2ca1d6b8d89f50595',
-      apiUrl: 'http://144.217.87.90:8000'
-  };
-  
+  production: true,
+  contractAddress: '0xc77008489f97994f29fd86b2ca1d6b8d89f50595',
+  apiUrl: 'http://144.217.87.90:8000'
+};
